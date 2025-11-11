cbddd0b65b5ccddcdb9bf593fe2dcd6a5be42079
Wellington <welltecnic@hotmail.com>
2025-11-10 21:54:13 -0300

    Codificando a controller usuario para login

--- ProjetoCrud2B/Controllers/UsuarioController.cs
+++ ProjetoCrud2B/Controllers/UsuarioController.cs
@@ -1,9 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoCrud2B.Repositorio;
+
+
 
 namespace ProjetoCrud2B.Controllers
 {
     public class UsuarioController : Controller
     {
+        //declaração
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+
+        //construtor
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+            //instância
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string senha)
+        {
+            var usuario = _usuarioRepositorio.ObterUsuario(email);
+
+            if(usuario != null && usuario.senha == senha)
+            {
+                return RedirectToAction("ListarProduto", "Produto");
+            }
+            ModelState.AddModelError("", "Email e Senha Inválidos");
+            return View();
+        }
+
+
+
+
         public IActionResult Index()
         {
             return View();
