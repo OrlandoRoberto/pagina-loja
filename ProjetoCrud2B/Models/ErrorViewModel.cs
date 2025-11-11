e3dc59889f03ec587141597c6ab106c89381caf5
Wellington <welltecnic@hotmail.com>
2025-11-10 21:18:23 -0300

    Criando projeto CRUD(Create,Read,Update,Delete)

---
+++ ProjetoCrud2B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoCrud2B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
